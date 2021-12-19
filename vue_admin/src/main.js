a13fe6f99772104c5734287bbad80ad9243bf1ec
zc Zhou <303368764@qq.com>
2021-12-19 21:46:09 +0800

    提交登陆功能

--- vue_admin/src/main.js
+++ vue_admin/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:5000/api/auth'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
 
 new Vue({
   router,
