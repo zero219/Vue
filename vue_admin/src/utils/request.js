effd32d4db175ff6545f0f6f116234fd3e0d7ce3
zero219 <303368764@qq.com>
2023-10-02 14:40:46 +0800

    1、请求出错时跳转提示页面

--- vue_admin/src/utils/request.js
+++ vue_admin/src/utils/request.js
@@ -1,7 +1,7 @@
 /* 封装axios用于发送请求 */
 import axios from 'axios'
 import router from '../router'
-import { getInfo, removeInfo } from './storage'
+import { getInfo } from './storage'
 
 // 创建一个新的axios实例
 const request = axios.create({
@@ -20,11 +20,9 @@ request.interceptors.request.use(
     if (token) {
       config.headers.Authorization = token
     }
-    // console.log(config)
     return config
   },
   function (error) {
-    // console.log('请求拦截器', error)
     // 对请求错误做些什么
     return Promise.reject(error)
   }
@@ -38,13 +36,26 @@ request.interceptors.response.use(
   },
   function (error) {
     if (error.response && error.response.status === 401) {
-      router.push('/Results')
-    }
-    if (error.message === 'Network Error') {
-      removeInfo('userInfo')
+      router.push({ name: 'Results', params: { msg: '抱歉,您暂时没有权限' } })
+    } else if (error.response && error.response.status === 500) {
+      router.push({
+        name: 'Results',
+        params: { msg: '服务器发生错误，请稍后重试' },
+      })
+    } else if (error.request) {
+      // 请求发送失败，没有收到响应
+      router.push({
+        name: 'Results',
+        params: { msg: '请求发送失败，请检查您的网络连接或稍后重试' },
+      })
+    } else {
+      // 其他错误
+      router.push({
+        name: 'Results',
+        params: { msg: '发生错误：' + error.message },
+      })
     }
     // 对响应错误做点什么
-    // console.log('响应拦截器', error.message)
     return Promise.reject(error)
   }
 )
--- vue_admin/src/views/results/index.vue
+++ vue_admin/src/views/results/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-result icon="warning" title="警告提示" subTitle="抱歉,您暂时没有权限">
+    <el-result icon="warning" title="警告提示" :subTitle="msg">
       <template slot="extra">
         <el-button type="primary" size="medium" @click="returnHandle"
           >返回</el-button
@@ -13,7 +13,12 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      msg: '抱歉,暂时出错了！！！',
+    }
+  },
+  created() {
+    this.msg = this.$route.params.msg
   },
   methods: {
     returnHandle() {
